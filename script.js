76056b48911dda6e0cf68f8ddcf99671384029bb
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-09-21 01:58:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+const browserName =navigator.appName;
+const version = navigator.appVersion;
+const element = document.getElementById("browser-info");
+element.innerHTML = "You are using " + browserName + " version " + version;
